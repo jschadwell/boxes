9a15f75a10ee48c0ecf695803b3d2ae81a75baec
Jeff Chadwell <jeffrey@chadwell.org>
2020-08-05 13:57:59 -0700

    Add code to validate input file type

--- Makefile
+++ Makefile
@@ -2,8 +2,9 @@ CXX=g++
 RM=rm -f
 #CPPFLAGS=-g $(shell root-config --cflags)
 CPPFLAGS=-I/usr/local/boost_1_73_0
+CXXFLAGS=-std=c++14
 #LDFLAGS=-g $(shell root-config --ldflags)
-LDFLAGS=
+LDFLAGS=-lboost_filesystem -L/usr/local/boost_1_73_0/stage/lib -Wl,-rpath,/usr/local/boost_1_73_0/stage/lib
 #LDLIBS=$(shell root-config --libs)
 
 SRCS=nest.cpp box.cpp
--- nest.cpp
+++ nest.cpp
@@ -1,11 +1,33 @@
 #include "box.h"
 #include <iostream>
+#include <string>
+#include <boost/filesystem.hpp>
+
+namespace filesys = boost::filesystem;
 
 int main(int argc, char* argv[]) {
     // Check the command line arguments
     if (argc != 2) {
         // Display usage message and exit
         std::cerr << "Usage: " << argv[0] << " config_file" << std::endl;
+        return 1;
+    }
+
+    try {
+        // Check to make sure the input file exists
+        filesys::path filepath(argv[1]);
+        if (!filesys::exists(filepath)) {
+            std::cerr << argv[0] << ": " << argv[1] << ": No such file" << std::endl;
+            return 1;
+        }
+        // Check to make sure the input file is the correct type
+        if (!filesys::is_regular_file(filepath)) {
+            std::cerr << argv[0] << ": " << argv[1] << ": Invalid file type" << std::endl;
+            return 1;
+        }
+    } catch (filesys::filesystem_error& e) {
+        // Something bad happened
+        std::cerr << e.what() << std::endl;
         return 1;
     }
 

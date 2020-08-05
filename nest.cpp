a11afd7f263334a6130748a3dbde5143971ca096
Jeff Chadwell <jeffrey@chadwell.org>
2020-08-05 11:50:52 -0700

    Add command-line argument processing

--- nest.cpp
+++ nest.cpp
@@ -1,6 +1,16 @@
 #include "box.h"
+#include <iostream>
 
 int main(int argc, char* argv[]) {
+    // Check the command line arguments
+    if (argc != 2) {
+        // Display usage message and exit
+        std::cerr << "Usage: " << argv[0] << " config_file" << std::endl;
+        return 1;
+    }
+
+    std::cout << "Input file: " << argv[1] << std::endl;
+
     Box box;
 
     box.print();

38a0d42e7ac53764fc56f5699f84b43e36f80289
Jeff Chadwell <jeffrey@chadwell.org>
2020-08-21 10:45:50 -0700

    Add ReorganizeBoxVisitor to main

--- nest.cpp
+++ nest.cpp
@@ -26,44 +26,5 @@ int main(int argc, char* argv[]) {
     ReorganizeBoxVisitor reorganizer;
     topBox->accept(reorganizer);
 
-
-#if 0
-    // Get configuration from config file
-    BoxConfigurator boxConfigurator;
-    std::vector<BoxConfigurationPtr> boxConfig;
-    if (!boxConfigurator.loadConfig(configFile, boxConfig)) {
-        return 1;
-    }
-
-    // Create boxes
-    BoxRepository boxRepository;
-    for (auto&& item : boxConfig) {
-        boxRepository.addBox(item->getId());
-    }
-
-    // Set up parent/child relationships in boxes
-    for (auto&& item : boxConfig) {
-        for (auto childIter = (*item).begin(); childIter != (*item).end(); childIter++) {
-            auto parent = boxRepository.getBox(item->getId());
-            auto child = boxRepository.getBox(*childIter);
-            parent->addChild(child);
-            child->addParent(parent);
-        }
-    }
-
-    // Find top-level box
-    Box* top = nullptr;
-    for (auto&& item : boxConfig) {
-        if (!boxRepository.getBox(item->getId())->getParent()) {
-            top = boxRepository.getBox(item->getId());
-            break;
-        }
-    }
-
-    top->print();
-    top->resize();
-    top->print();
-
-#endif
     return 0;
 }
